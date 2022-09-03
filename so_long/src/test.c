c3b2e2fdafa21a52608e5bf4ff4b2d757bf8ba99
Nugah <fwong@e2r11p2.clusters.42paris.fr>
2022-09-03 18:49:45 +0200

    release

---
+++ so_long/src/test.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 01:05:30 by fwong             #+#    #+#             */
+/*   Updated: 2022/08/26 01:05:30 by fwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include "../minilibx-linux/mlx.h"
+
+#define MLX_ERROR 1
+
+int main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
+	win_ptr = mlx_new_window(mlx_ptr, 500, 800, "My first window!");
+	if (win_ptr == NULL)
+	{
+		free(win_ptr);
+		return (MLX_ERROR);
+	}
+	while (1)
+		;
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+}
