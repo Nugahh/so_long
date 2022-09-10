edb5013fe5d37923477cba780a30d01a96eb6571
nugahh <nugah@DESKTOP-96KGKIL.localdomain>
2022-09-10 03:45:28 +0200

    release

--- so_long/obj/check_map.d
+++
@@ -1,9 +0,0 @@
-obj/check_map.o: src/parsing/check_map.c \
-  src/parsing/../../includes/so_long.h includes/../libft/libft.h \
-  includes/../libft/get_next_line.h
-
-src/parsing/../../includes/so_long.h:
-
-includes/../libft/libft.h:
-
-includes/../libft/get_next_line.h:
